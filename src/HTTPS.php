a781a55a31e02e9d7a32963681d331bfe6cf7d86
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-15 02:29:14 -0500

    Update HTTPS.php

--- src/HTTPS.php
+++ src/HTTPS.php
@@ -13,6 +13,7 @@ class HTTPS
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0); // Use HTTP/2.0
+        curl_setopt($ch, CURLOPT_ENCODING, ''); // Enable all supported encodings
         $html = curl_exec($ch);
         curl_close($ch);
         return $html;
