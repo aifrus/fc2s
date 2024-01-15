b672f957f41f42a528707cbf174a0d9b73faa670
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-15 02:20:07 -0500

    x

--- src/FetchFAA.php
+++ src/FetchFAA.php
@@ -6,9 +6,12 @@ class FetchFAA
 {
     const HOME_PAGE = 'https://www.faa.gov/air_traffic/flight_info/aeronav/aero_data/NASR_Subscription/';
     const DATA_FILE = 'https://nfdc.faa.gov/webContent/28DaySub/extra/DD_Mon_YYYY_CSV.zip';
+    const HEADERS = [
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    ];
 
     public static function get_home_page_html()
     {
-        return HTTPS::get(self::HOME_PAGE);
+        return HTTPS::get(self::HOME_PAGE, self::HEADERS);
     }
 }
--- src/HTTPS.php
+++ src/HTTPS.php
@@ -6,7 +6,6 @@ class HTTPS
 {
     public static function get($url, $headers = [])
     {
-        $headers[] = 'User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36';
         return file_get_contents($url, false, stream_context_create([
             'http' => [
                 'method' => 'GET',
