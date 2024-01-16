a7bc6b3e026ad8a11165be3fcdd29c0bf4e63112
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-16 07:38:52 -0500

    Update describe.php

--- example/describe.php
+++ example/describe.php
@@ -13,14 +13,14 @@ echo "----\n";
 foreach ($tables as $table) {
     $tableName = $table[0];
 
-    echo "Description of table {$tableName}:\n";
+    echo "Table {$tableName}:\n";
     $description = $mysqli->query("DESCRIBE `{$tableName}`");
     while ($row = $description->fetch_assoc()) echo implode(",", $row) . "\n";
-    echo "\nRandom Examples from {$tableName}:\n";
-    $randomRows = $mysqli->query("SELECT * FROM `{$tableName}` ORDER BY RAND() LIMIT 5");
-    while ($row = $randomRows->fetch_assoc()) {
-        echo implode(",", $row) . "\n";
-    }
+    // echo "\nRandom Examples from {$tableName}:\n";
+    // $randomRows = $mysqli->query("SELECT * FROM `{$tableName}` ORDER BY RAND() LIMIT 5");
+    // while ($row = $randomRows->fetch_assoc()) {
+    //     echo implode(",", $row) . "\n";
+    // }
     echo "----\n";
 }
 
