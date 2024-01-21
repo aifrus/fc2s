b488073c661a11da5a7ce3ee13242c87b2d2c333
Russell Purinton <russell.purinton@icloud.com>
2024-01-21 15:38:51 +0000

    push

--- example/describe.php
+++ example/describe.php
@@ -1,27 +1,13 @@
 <?php
-$mysqli = new mysqli("127.0.0.1", "aifr", "aifr", "NASR_2023-12-28");
-
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-    exit();
-}
-
-// Get all table names
-$result = $mysqli->query("SHOW TABLES");
-$tables = $result->fetch_all(MYSQLI_NUM);
-echo "----\n";
+$lines[] = 'TABLE,FIELD,TYPE,NULL,KEY,DEFAULT,EXTRA';
+$mysqli = new mysqli("127.0.0.1", "aifr", "aifr", "NASR_INDEX");
+if ($mysqli->connect_errno) die("Failed to connect to MySQL: " . $mysqli->connect_error);
+extract($mysqli->query("SELECT `name` FROM `INDEX` ORDER BY `id` DESC LIMIT 1")->fetch_assoc());
+$mysqli->select_db($name);
+$tables = $mysqli->query("SHOW TABLES")->fetch_all(MYSQLI_NUM);
 foreach ($tables as $table) {
-    $tableName = $table[0];
-
-    echo "Table {$tableName}:\n";
-    $description = $mysqli->query("DESCRIBE `{$tableName}`");
-    while ($row = $description->fetch_assoc()) echo implode(",", $row) . "\n";
-    echo "\nRandom Examples from {$tableName}:\n";
-    $randomRows = $mysqli->query("SELECT * FROM `{$tableName}` ORDER BY RAND() LIMIT 5");
-    while ($row = $randomRows->fetch_assoc()) {
-        echo implode(",", $row) . "\n";
-    }
-    echo "----\n";
+    $description = $mysqli->query("DESCRIBE `{$table[0]}`");
+    while ($row = $description->fetch_assoc()) $lines[] = $table[0] . ',' . implode(",", $row);
 }
-
 $mysqli->close();
+file_put_contents("describe.txt", implode("\n", $lines));
