af7279f80f388f5df919014f19882b20e5dc2f53
Russell Purinton <russell.purinton@icloud.com>
2024-01-25 16:37:14 +0000

    push

---
+++ example/get_current.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Aifrus\Fc2s;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$export_dir = __DIR__ . '/export';
+@mkdir($export_dir);
+
+$config = [
+    'host' => '127.0.0.1',
+    'user' => 'aifr',
+    'pass' => 'aifr',
+    'prefix' => 'NASR_',
+    'export_dir' => $export_dir,
+];
+
+// Get All
+//if (!Process::get_all($config)) die("Failed to process\n");
+//echo "Success\n";
+
+// Get Latest
+if (!Process::get_current($config)) die("Failed to process\n");
+echo "Success\n";
