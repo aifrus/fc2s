ab9847aab36b4fc7a24e4f18217df8736b76b409
Russell Purinton <russell.purinton@gmail.com>
2025-03-26 04:02:04 +0000

    2025-03-26 04:02:04

--- examples/get_current.php
+++ examples/get_current.php
@@ -19,7 +19,7 @@ if (!is_dir($exportDir) && !mkdir($exportDir, 0755, true)) {
 
 // Database configuration
 $config = [
-    'host' => '127.0.0.1',
+    'host' => 'mysql',
     'user' => 'nasr',
     'pass' => 'nasr',
     'prefix' => 'NASR_',
